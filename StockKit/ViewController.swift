cc539c32c4a3e127513c932466479f32950a12be
Nathan Tannar <nathantannar4@gmail.com>
2017-07-20 19:40:22 -0700

    Initial Commit

---
+++ StockKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockKit
+//
+//  Created by Nathan Tannar on 7/20/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
